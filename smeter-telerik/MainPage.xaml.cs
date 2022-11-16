3cf4f8934cccc53a5f582f47f7ed8e9666f2ea74
Wojciech Igielski <wojtekiglaa69@outlook.com>
2022-11-16 13:56:44 +0100

    wodafak dziala XDDD

--- smeter-telerik/MainPage.xaml.cs
+++ smeter-telerik/MainPage.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace smeter_telerik;
+﻿//using Telerik.UI.Xaml.Controls.DataVisualization;
+
+namespace smeter_telerik;
 
 public partial class MainPage : ContentPage
 {
+	Random random = new Random();
 	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	public async void Update()
+	{
+		await DisplayAlert("Leszek", "Korzan", "Webaily");
+		dzazgometr.Value = 69;
+	}
+
+
+    private async void Start_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Leszek", "Korzan", "Webaily");
+        dzazgometr.Value = random.Next(1,200);
+    }
 }
 
