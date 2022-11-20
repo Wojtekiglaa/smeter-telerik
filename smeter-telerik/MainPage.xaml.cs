5b8c9b6d65838c2d7f21e2cb0380a5f22f9392dc
Wojciech Igielski <wojtekiglaa69@outlook.com>
2022-11-20 23:18:47 +0100

    got it doneeeee

--- smeter-telerik/MainPage.xaml.cs
+++ smeter-telerik/MainPage.xaml.cs
@@ -1,27 +1,84 @@
 ﻿//using Telerik.UI.Xaml.Controls.DataVisualization;
-
 namespace smeter_telerik;
-
 public partial class MainPage : ContentPage
 {
 	Random random = new Random();
-	int count = 0;
+    bool czy = true;
+    int sus;
 
 	public MainPage()
 	{
 		InitializeComponent();
+        //dzazgometr.Value= 200;
+        sus = 10;
+        Vibrate();
+        Update();
 	}
+    public async void AccelerometerStart()
+    {
+        Accelerometer.Start(SensorSpeed.Fastest);
+        Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+    }
 	public async void Update()
 	{
-		await DisplayAlert("Leszek", "Korzan", "Webaily");
-		dzazgometr.Value = 69;
-	}
+        //Vibrate();
+        while (true)
+        {
+            if (czy)
+            {
+                await Task.Delay(1000);
+                //dzazgometr.Value = random.Next(1, 200);
+                dzazgometr.Value = sus;
+                sus -= 3;
+            }
+            else
+            {
+                dzazgometr.Value = 0;
+            }
+        }
+    }
 
-
-    private async void Start_Clicked(object sender, EventArgs e)
+    private async void Vibrate()
     {
-        await DisplayAlert("Leszek", "Korzan", "Webaily");
-        dzazgometr.Value = random.Next(1,200);
+        while (true)
+        {
+            if (DeviceInfo.Current.Platform == DevicePlatform.Android)
+            {
+                switch (sus)
+                {
+                    case <= 100: { while (sus <= 100) { Vibration.Default.Vibrate(100); await Task.Delay(700); } break; }
+                    case <= 200: { while (sus <= 200) { Vibration.Default.Vibrate(100); await Task.Delay(500); } break; }
+                    case <= 300: { while (sus <= 300) { Vibration.Default.Vibrate(100); await Task.Delay(300); } break; }
+                }
+            }
+        }
+    }
+    private void Start_Clicked(object sender, EventArgs e)
+    {
+       //czy = true;
+       sus += 15;
+    }
+
+    void Accelerometer_ShakeDetected(object sender,EventArgs args )
+    {
+        sus += 15;
+    }
+
+    private void Stop_Clicked(object sender, EventArgs e)
+    {
+		//czy = false;
+       Accelerometer.Stop();
+       sus -= 15;
+    }
+
+    private void Stop_Accel(object sender, EventArgs e)
+    {
+        Accelerometer.Stop();
+    }
+
+    private void Start_Accel(object sender, EventArgs e)
+    {
+        AccelerometerStart();
     }
 }
 
---
+++ smeter-telerik/NewPage1.xaml.cs
@@ -0,0 +1,33 @@
+namespace smeter_telerik;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+        ReadDeviceInfo();
+	}
+    private void ReadDeviceInfo()
+    {
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        sb.AppendLine($"Model: {DeviceInfo.Current.Model}");
+        sb.AppendLine($"Manufacturer: {DeviceInfo.Current.Manufacturer}");
+        sb.AppendLine($"Name: {DeviceInfo.Name}");
+        sb.AppendLine($"OS Version: {DeviceInfo.VersionString}");
+        sb.AppendLine($"Refresh Rate: {DeviceInfo.Current}");
+        sb.AppendLine($"Idiom: {DeviceInfo.Current.Idiom}");
+        sb.AppendLine($"Platform: {DeviceInfo.Current.Platform}");
+
+        bool isVirtual = DeviceInfo.Current.DeviceType switch
+        {
+            DeviceType.Physical => false,
+            DeviceType.Virtual => true,
+            _ => false
+        };
+
+        sb.AppendLine($"Virtual device? {isVirtual}");
+
+        Devinfo.Text = sb.ToString();
+    }
+}
